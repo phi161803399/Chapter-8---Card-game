0c273593583a883f57c377f5c44b11fab6592476
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-05-31 13:34:57 +0200

    finished

---
+++ Chapter 8 - Card game/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_8___Card_game
+{
+    class Card
+    {
+        public Suits Suit { get; private set; }
+        public Values Value { get; private set; }
+
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- Chapter 8 - Card game/Form1.cs
+++ Chapter 8 - Card game/Form1.cs
@@ -12,9 +12,16 @@ namespace Chapter_8___Card_game
 {
     public partial class Form1 : Form
     {
+        private Random random;
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
+            int numberBetween0and3 = random.Next(4);
+            int numberBetween1and13 = random.Next(1,14);
+            Card card = new Card((Suits)numberBetween0and3,(Values)numberBetween1and13);
+            string cardname = card.Name;
+            MessageBox.Show(cardname);
         }
     }
 }
